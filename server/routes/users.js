4f2feaa10a1f03f102b30ba6c856746674054e84
Dong Quang Quy <quangquya2@gmail.com>
2021-12-02 20:36:26 +0700

    fix - login

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const userController = require('../app/controllers/userController');
 const auth = require('../app/middleware/auth');
 
 router.get('/me', auth, userController.getMe);
-router.post('/register', userController.register);
+router.post('/login', userController.login);
 router.post('/register', userController.register);
 router.put('/me/name', auth, userController.updateName);
 router.put('/me/information', auth, userController.updateInformation);
